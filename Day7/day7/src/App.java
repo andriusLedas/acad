d212bf9bc94bd3dbfef2dbbea38426b129d8bbe0
andriusLedas <152397675+andriusLedas@users.noreply.github.com>
2024-03-13 16:24:15 +0200

    Update App.java
    Feature: print "Hello to Everybody!"

--- Day7/day7/src/App.java
+++ Day7/day7/src/App.java
@@ -1,9 +1,7 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-
         System.out.println("Hello to you too!");
-
+        System.out.println("Hello to everybody!");
     }
-
 }
